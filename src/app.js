7ae92f80ff95c102cb9c01162d5354a21a79e27a
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-06-18 15:20:56 +0300

    create state & add lib on change

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ class App {
 			view: MainView,
 		},
 	];
+	appState = {
+		favorites: [],
+	};
 
 	constructor() {
 		window.addEventListener('hashchange', this.route.bind(this));
@@ -20,7 +23,7 @@ class App {
 
 		const view = this.routes.find((r) => r.path === location.hash).view;
 
-		this.currentView = new view();
+		this.currentView = new view(this.appState);
 		this.currentView.render();
 	}
 }
